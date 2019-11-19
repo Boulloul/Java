962034b17437cb9fce8ae9c31b55adc058245a0b
ABYADE <khadija.abyade20@gmail.com>
2019-11-19 10:31:21 +0000

    tp2

--- TP2/src/Exercice1/Compte.java
+++ TP2/src/Exercice1/Compte.java
@@ -20,8 +20,9 @@ public class Compte implements Runnable {
 				for(int i=1000;i>=1;i--) {
 					System.out.println(nom+" : "+valeur+" ");
 					valeur--;
+					Thread.sleep(1000);
 				}
-						Thread.sleep(1000);
+						
 				
 			}
 			} catch (InterruptedException e) {
